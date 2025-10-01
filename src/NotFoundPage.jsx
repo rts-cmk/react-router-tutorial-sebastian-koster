11621716cb8fe6c24907279dea075ad66996ba80
kosterseb <sebkos.code@gmail.com>
2025-10-01 10:32:52 +0200

    added a pagenotfound function that will bring you back to the welcomepage

--- src/NotFoundPage.jsx
+++ src/NotFoundPage.jsx
@@ -1,8 +1,15 @@
-export default function NotFoundPage() {
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => {
     return (
         <div className="page">
-        <h1>404 - Page Not Found</h1>
-        <p>The page you are looking for does not exist.</p>
+            <h1>404 - Page Not Found</h1>
+            <p>The page you are looking for does not exist.</p>
+            <Link to="/welcome">
+            <button>Go back to the Welcome Page</button>
+            </Link>
         </div>
     );
-    }
+};
+
+export default NotFoundPage;
