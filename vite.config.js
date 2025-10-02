108659c8e9cb375ec2029c32bdf76373effcc52a
kosterseb <sebkos.code@gmail.com>
2025-10-02 12:39:25 +0200

    fixing routing

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/react-router-tutorial-sebastian-koster/',
+  build: {
+    outDir: 'docs'
+  }
 })
