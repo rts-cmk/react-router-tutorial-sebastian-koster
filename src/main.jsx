521f613c1c1ee6553840cdf30833dd36d2f5f1a0
kosterseb <sebkos.code@gmail.com>
2025-10-03 08:49:52 +0200

    im trying Hash instead of browsing for router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
 // Pages
@@ -14,7 +14,7 @@ import WhatIsRouter from './WhatIsRouterPage.jsx'
 import NotFoundPage from './NotFoundPage.jsx'
 import App from './App.jsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {path: '/', element: <App />},
   {path: '/welcome', element: <Welcome />},
   {path: '/what-is-router', element: <WhatIsRouter />},
